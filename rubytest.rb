10223fbcdbfc1d3dc49fd04c24dc9d3dd20d5f30
Martin Page <wookoouk@gmail.com>
2016-05-12 10:05:16 +0100

    ensure free

--- rubytest.rb
+++ rubytest.rb
@@ -6,19 +6,24 @@ require 'json'
 module LibPP
   extend FFI::Library
   ffi_lib './pp.so'
-  attach_function :ProcessInRuby, [:string],:pointer
+  attach_function :ProcessInRuby, [:string], :pointer
   attach_function :free, [:pointer], :void
 end
 
-opts ={}
-opts[:file] = "/Users/pagem/Documents/workspace/pileup-processor/test.pileup"
-opts[:ignore_reference_n] = true
-opts[:min_depth] = 6
-opts[:min_non_ref_count] = 3
 
-puts "RUBY: " + opts.to_json
+begin
+  opts ={}
+  opts[:file] = "/Users/pagem/Documents/workspace/pileup-processor/test.pileup"
+  opts[:ignore_reference_n] = true
+  opts[:min_depth] = 6
+  opts[:min_non_ref_count] = 3
 
-out_pointer =  LibPP.ProcessInRuby(opts.to_json)
-puts out_pointer.read_string
+  puts "RUBY: " + opts.to_json
 
-LibPP.free(out_pointer) if out_pointer
+  out_pointer = LibPP.ProcessInRuby(opts.to_json)
+  puts out_pointer.read_string
+
+ensure
+  LibPP.free(out_pointer) if out_pointer
+
+end
